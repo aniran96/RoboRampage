e7adb42f98a0bdf097e3d3c08b007da67b1bfd6b
aniran <aniran530@gmail.com>
2025-03-15 01:31:59 +0530

    Implement enemy provocation logic: add aggro distance and conditionally set target position based on player proximity

--- Scripts/Characters/Enemy/Enemy.cs
+++ Scripts/Characters/Enemy/Enemy.cs
@@ -6,11 +6,15 @@ public partial class Enemy : CharacterBody3D
 	[Export]
 	private NavigationAgent3D _enemyNavigationAgent3DNode;
 	
-	
+	// constants
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
-
+	
+	// variables
 	private Player _player;
+
+	private bool _isProvoked = false;
+	private float _aggroDistance = 12.0f;
 
     public override void _Ready()
     {
@@ -19,13 +23,24 @@ public partial class Enemy : CharacterBody3D
 
     public override void _Process(double _delta)
     {
-        _enemyNavigationAgent3DNode.TargetPosition = _player.GlobalPosition;
+		if (_isProvoked == true) 
+		{ 
+			_enemyNavigationAgent3DNode.TargetPosition = _player.GlobalPosition; 
+		}
+
     }
 
     public override void _PhysicsProcess(double delta)
 	{
 		var nextPathPosition = _enemyNavigationAgent3DNode.GetNextPathPosition();
 		Vector3 velocity = Velocity;
+
+		var distance = GlobalPosition.DistanceTo(_player.GlobalPosition);
+
+		if (distance <= _aggroDistance) 
+		{ 
+			_isProvoked = true; 
+		}
 
 		var direction = GlobalPosition.DirectionTo(nextPathPosition);
 
